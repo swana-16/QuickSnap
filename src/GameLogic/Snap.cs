e9e0e856fde1e9ba555c644923db79b60e786fe7
swana-16 <4323904@students.swinburne.edu.my>
2016-09-08 16:38:27 +0800

    Update Snap.cs

--- src/GameLogic/Snap.cs
+++ src/GameLogic/Snap.cs
@@ -131,19 +131,22 @@ namespace CardGames.GameLogic
 		/// Check if the top two cards' ranks match.
 		/// </summary>
 		public void PlayerHit (int player)
-		{
-			//TODO: consider deducting score for miss hits???
-			if ( player >= 0 && player < _score.Length &&  	// its a valid player
-				 IsStarted && 								// and the game is started
-				 _topCards [0] != null && _topCards [0].Rank == _topCards [1].Rank) // and its a match
-			{
-				_score[player]++;
-				//TODO: consider playing a sound here...
-			}
-
-			// stop the game...
-			_started = false;
-		}
+ {
+ //TODO: consider deducting score for miss hits???
+ if ( player >= 0 && player < _score.Length && // …
+ IsStarted && // …
+ _topCards[0] != null &&
+ _topCards[0].Rank == _topCards [1].Rank) // …
+ {
+ _score[player]++;
+ }
+ else if ( player >= 0 && player < _score.Length)
+ {
+ _score[player]--;
+ }
+ // stop the game...
+ _started = false;
+ }
 	
 		#region Snap Game Unit Tests
 		#if DEBUG
